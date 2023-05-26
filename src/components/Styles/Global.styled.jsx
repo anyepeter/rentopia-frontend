30cbcee39dacd0afd302ddcd97bd400453b15813
anyepeter <petzyrockchendi@gmail.com>
2023-05-26 21:32:52 +0100

    add navbar menu

---
+++ src/components/Styles/Global.styled.jsx
@@ -0,0 +1,11 @@
+
+export const theme = {
+    colors: {
+        primary: '#17C788',
+        secondary: '#0d1432',
+    },
+    fonts:{
+        primary: 'Mulish, sans-serif',
+        secondary: 'Comfortaa, cursive',
+    }
+}
