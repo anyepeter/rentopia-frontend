5bed354804a11e904256dde50f3b3445ef94d78a
anyepeter <petzyrockchendi@gmail.com>
2023-07-25 23:43:17 +0100

    add submit form file

--- src/Redux/houses/houseSlice.jsx
+++ src/Redux/houses/houseSlice.jsx
@@ -1,8 +1,10 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const GET_HOUSES = "redux/houses";
+const GET_HOUSES = "redux/houses/";
+const POST_HOUSES = "redux/houses/add";
 
+// Return all the houses
 export const fetchHouses = createAsyncThunk(GET_HOUSES, async () => {
   try {
     const response = await axios.get('http://127.0.0.1:3000/houses');
@@ -11,6 +13,26 @@ export const fetchHouses = createAsyncThunk(GET_HOUSES, async () => {
     throw error;
   }
 });
+
+//add a house
+export const addHouse = createAsyncThunk(POST_HOUSES, async (data, thunkAPI) => {
+  // const token = localStorage.getItem('token');
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'multipart/form-data',
+      // Authorization: `Bearer ${token}`,
+    },
+  };
+  try {
+    return await axios.post('http://127.0.0.1:3000/houses', data, requestOptions);
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response.data.error);
+  }
+});
+
+
+
 
 const initialState = {
   houses: [],
@@ -45,6 +67,25 @@ const houseSlice = createSlice({
         success: false,
         error: action.error.message,
       }));
+     
+      //add a house
+      reduce
+      .addCase(addHouse.pending, (state) => ({
+        ...state,
+        isLoading: true,
+        error: '',
+      }))
+      .addCase(addHouse.fulfilled, (state, action) => ({
+        ...state,
+        isLoading: false,
+        success: true,
+        response: action.payload.data.data,
+      }))
+      .addCase(addHouse.rejected, (state) => ({
+        ...state,
+        isLoading: false
+      }));
+
   }
 });
 
